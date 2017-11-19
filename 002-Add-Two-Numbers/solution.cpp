4849ed102e2c3ad74873b651856c2fe378f43b12
康永胜 <kys8823@gmail.com>
2017-11-19 17:35:29 +0800

    改变策略，增加测试用例

--- 002-Add-Two-Numbers/solution.cpp
+++ 002-Add-Two-Numbers/solution.cpp
@@ -20,31 +20,24 @@ ListNode * Solution::addTwoNumbers(ListNode* l1, ListNode* l2){
     int carry = 0, num = 0;
 	ListNode *p1, *p2, *p3, *result = NULL;
 	
-	for(p1 = l1, p2 = l2; p1 != NULL && p2 != NULL; p1 = p1->next, p2 = p2->next){
-		num = p1->val + p2->val + carry;
+	for(p1 = l1, p2 = l2; p1 != NULL || p2 != NULL || carry != 0;){
+		num = 0;
+		
+		if(p1 != NULL){
+			num += p1->val;
+			p1 = p1->next;
+		}
+		
+		if(p2 != NULL){
+			num += p2->val;
+			p2 = p2->next;
+		}
+		
+		
+		num += carry;
 		carry = num / 10;
 		num   = num % 10;
 
-		if(result == NULL){
-			p3 = result = new ListNode(num);
-		}else{
-			p3->next = new ListNode(num);
-			p3 = p3->next;
-		}
-	}
-	
-	
-	p1 = p2 != NULL ? p2 : p1;
-	
-	if(p1 == NULL){
-		return result;
-	}
-	
-	for(; p1 != NULL; p1 = p1->next){
-		num = p1->val + carry;
-		carry = num / 10;
-		num   = num % 10;
-		
 		if(result == NULL){
 			p3 = result = new ListNode(num);
 		}else{
@@ -90,6 +83,17 @@ int main(void){
 	freeList(l3);
 	
 	cout << "测试通过" << endl;
+	
+	l1 = new ListNode(5);
+	l2 = new ListNode(5);
+	l3 = s1.addTwoNumbers(l1, l2);
+	
+	assert(l3->val == 0);
+	assert(l3->next->val == 1);
+	
+	freeList(l1);
+	freeList(l2);
+	freeList(l3);
 	
 	return 0;
 }
