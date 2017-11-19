d065c7e7288c890f29681396d1df4d72c21c9aa2
康永胜 <kys8823@gmail.com>
2017-11-19 17:25:28 +0800

    题目编写完成

---
+++ 002-Add-Two-Numbers/solution.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void freeList(ListNode *);
+
+class Solution {
+	public:
+	    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
+};
+
+ListNode * Solution::addTwoNumbers(ListNode* l1, ListNode* l2){
+    int carry = 0, num = 0;
+	ListNode *p1, *p2, *p3, *result = NULL;
+	
+	for(p1 = l1, p2 = l2; p1 != NULL && p2 != NULL; p1 = p1->next, p2 = p2->next){
+		num = p1->val + p2->val + carry;
+		carry = num / 10;
+		num   = num % 10;
+
+		if(result == NULL){
+			p3 = result = new ListNode(num);
+		}else{
+			p3->next = new ListNode(num);
+			p3 = p3->next;
+		}
+	}
+	
+	
+	p1 = p2 != NULL ? p2 : p1;
+	
+	if(p1 == NULL){
+		return result;
+	}
+	
+	for(; p1 != NULL; p1 = p1->next){
+		num = p1->val + carry;
+		carry = num / 10;
+		num   = num % 10;
+		
+		if(result == NULL){
+			p3 = result = new ListNode(num);
+		}else{
+			p3->next = new ListNode(num);
+			p3 = p3->next;
+		}
+	}
+	
+	return result;	
+}
+
+int main(void){
+	ListNode *l1, *l2, *l3;
+	Solution s1;
+	
+	l1 = new ListNode(2);
+	l1->next = new ListNode(4);
+	l1->next->next = new ListNode(3);
+	
+	l2 = new ListNode(5);
+	l2->next = new ListNode(6);
+	l2->next->next = new ListNode(4);
+	
+	l3 = s1.addTwoNumbers(l1, l2);
+	
+	assert(l3->val == 7);
+	assert(l3->next->val == 0);
+	assert(l3->next->next->val == 8);
+	
+	freeList(l3);
+	
+	l2->next->next->next = new ListNode(4);
+	
+	l3 = s1.addTwoNumbers(l1, l2);
+	
+	assert(l3->val == 7);
+	assert(l3->next->val == 0);
+	assert(l3->next->next->val == 8);
+	assert(l3->next->next->next->val == 4);
+	
+	freeList(l1);
+	freeList(l2);
+	freeList(l3);
+	
+	cout << "测试通过" << endl;
+	
+	return 0;
+}
+
+void freeList(ListNode *l){
+	ListNode * curr;
+	while(l != NULL){
+		cout << "free " << l->val << endl;
+		curr = l;
+		l = l->next;
+		delete curr;
+	}
+}
